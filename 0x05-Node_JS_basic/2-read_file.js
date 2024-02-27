875ab28a00a3c54991626e7be96ae4f13b8d9413
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-27 21:20:51 +0300

    remove verbatim in eror

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -32,7 +32,7 @@ function countStudents(filename) {
       }
     }
   } catch (error) {
-    console.error('Cannot load the database', error.message);
+    console.error('Cannot load the database');
   }
 }
 
