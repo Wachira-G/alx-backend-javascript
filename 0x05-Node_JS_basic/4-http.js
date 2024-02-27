851d8a78df40312712e8763d4a7d8dec5e6122b5
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-27 22:29:18 +0300

    add task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const app = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const server = app.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(port, hostname, () => {});
+
+module.export = app;
