c81e3df40ab3f198952202c06320be6d9c870357
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-29 17:46:08 +0300

    add it block for each assert

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -7,37 +7,50 @@ const calculateNumber = require('./1-calcul');
 describe('calculateNumber', () => {
   describe('#SUM', () => {
     it('should return the sum of rounded numbers', () => {
-
       assert.strictEqual(calculateNumber('SUM', 1, 1), 2);
+    });
 
+    it('should return the sum of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUM', 3.7, 2.3), 6);
+    });
 
+    it('should return the sum of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUM', 5.1, 1.9), 7);
     });
   });
 
   describe('#SUBTRACT', () => {
     it('should return the difference of rounded numbers', () => {
-
       assert.strictEqual(calculateNumber('SUBTRACT', 1, 1), 0);
+    });
 
+    it('should return the difference of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
+    });
 
+    it('should return the difference of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 3.1, 1.1), 2);
+    });
 
+    it('should return the difference of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.1, 3.0), -2);
     });
   })
 
   describe('#DIVIDE', () => {
     it('should return the result of division of rounded numbers', () => {
-
       assert.strictEqual(calculateNumber('DIVIDE', 1, 2), 0.5);
+    });
 
+    it('should return the result of division of rounded numbers', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1, 0.2), 'Error');
+    });
 
+    it('should return the result of division of rounded numbers', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1, 0.5), 1);
+    });
 
+    it('should return the result of division of rounded numbers', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.1, 3.0), 0.3333333333333333);
     });
   });
