e7f4782f68243a27de2d326ea15dfb4719665bc7
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-29 23:51:35 +0300

    add task 9 files

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('should return the correct status code and result for GET /', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return the correct status code for a valid cart ID', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return a 404 status code for an invalid cart ID', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
