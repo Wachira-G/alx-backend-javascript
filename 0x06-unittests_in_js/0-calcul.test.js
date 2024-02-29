e385d7044a02d3447d7de82f4f60bf4e97bd11fc
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-29 22:22:08 +0300

    add some tests to task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,6 +7,8 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should return the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(3.7, 2.3), 6);
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
   });
 
   it('should return the sum of rounded numbers', () => {
