9922eddc59e3102aca6fcff602fbccc83c03521c
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-29 22:39:24 +0300

    add task 4 files

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
+
